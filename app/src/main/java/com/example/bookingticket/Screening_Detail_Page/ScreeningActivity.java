5d5d0e0ddcdcfe8dd2336e1e8df3942664537144
duckies <voducminh39@gmail.com>
2024-05-22 15:37:35 +0700

    init pakage

---
+++ app/src/main/java/com/example/bookingticket/Screening_Detail_Page/ScreeningActivity.java
@@ -0,0 +1,16 @@
+package com.example.bookingticket.Screening_Detail_Page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bookingticket.R;
+
+public class ScreeningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screening);
+    }
+}
