c642e7b4015e6e518089cd123cadfadf4ce5ff76
Cao Văn Vượng <146173412+CaoVanVuongDH21DTA@users.noreply.github.com>
2024-05-23 21:36:03 +0700

    them trang thanh toan va trang setting

--- app/src/main/java/com/example/bookingticket/Screening_Detail_Page/ScreeningActivity.java
+++ app/src/main/java/com/example/bookingticket/Screening_Detail_Page/ScreeningActivity.java
@@ -2,16 +2,33 @@ package com.example.bookingticket.Screening_Detail_Page;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.example.bookingticket.MainActivity;
 import com.example.bookingticket.R;
 
 public class ScreeningActivity extends AppCompatActivity {
+
+    public ImageView backpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screening);
 
+        backpage = findViewById(R.id.btn_backpage);
+
+        backpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScreeningActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
